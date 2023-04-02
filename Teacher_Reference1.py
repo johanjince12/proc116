48c72e063e490526a9d5db68a73e25fcc6f49cb8
johanjince12 <96847741+johanjince12@users.noreply.github.com>
2023-04-02 10:27:06 -0500

    Add files via upload

---
+++ Teacher_Reference1.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask (__name__)
+
+@app.route("/")
+
+def first_flask():
+    
+    return "this is my first flask program"
+
+
+
+@app.route("/flask_2")
+def second_flask():
+    return "Python is fun"
+
+app.run(debug=True)
